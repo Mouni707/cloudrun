4b794c48cc22ca329814559037982243f0fa084d
mounika <mounika.sakam707@gmail.com>
2026-07-15 21:36:22 +0530

    Update greeting message in main.py for personalization

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from flask import Flask
 
   @app.route("/")
   def hello():
-      return "Hello World from Cloud Run via GitHub Actions!"
+      return "diwakar from Cloud Run via GitHub Actions!"
 
   if __name__ == "__main__":
       app.run(host="0.0.0.0", port=8080)
